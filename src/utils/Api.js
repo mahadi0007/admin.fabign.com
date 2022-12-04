20dfca9fc1a710343cf0413caac797f3a9ee2169
SirRatul <samsulratul98@gmail.com>
2022-12-04 22:16:40 +0600

    api url change

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,10 +1,9 @@
-// export const AuthAPI = "https://c98f-103-126-218-161.ngrok.io/api/v1/auth/";
-export const AuthAPI = "http://localhost:4010/api/v1/auth/";
-// export const AclAPI = "https://c98f-103-126-218-161.ngrok.io/api/v1/";
-export const AclAPI = "http://localhost:4010/api/v1/";
-// export const API = "https://c98f-103-126-218-161.ngrok.io/api/v1/admin/";
-export const API = "http://localhost:4010/api/v1/admin/";
-// export const ECommerceApi =
-// "https://c98f-103-126-218-161.ngrok.io/api/v1/e-efg/";
-export const ECommerceApi = "http://localhost:4010/api/v1/e-efg/";
+export const AuthAPI = "http://164.92.119.47/api/v1/auth/";
+// export const AuthAPI = "http://localhost:4010/api/v1/auth/";
+export const AclAPI = "http://164.92.119.47/api/v1/";
+// export const AclAPI = "http://localhost:4010/api/v1/";
+export const API = "http://164.92.119.47/api/v1/admin/";
+// export const API = "http://localhost:4010/api/v1/admin/";
+export const ECommerceApi = "http://164.92.119.47/api/v1/e-efg/";
+// export const ECommerceApi = "http://localhost:4010/api/v1/e-efg/";
 export const SmsApi = "https://api.sms.net.bd/";
